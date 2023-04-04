0fd7858579afea35a72f3ef6a322a76892ccb45c
Aurore DUCAP <aurore.ducap@gmail.com>
2023-04-04 17:27:16 +0400

    table_creation mod + insert_data

---
+++ insert_data.sql
@@ -0,0 +1,23 @@
+-- Ajoutez 5 entrepôts dans différentes villes et pays.
+insert into entrepots (nom_entrepot, adresse, ville, pays)
+	values
+		("ent_lux", "234 route Arlon", "Strassen", "Luxembourg"),
+        ("ent_pto", "555 rua du Bacalau", "Porto", "Portugal"),
+        ("ent_ans", "346 rue de Charlemagne", "Angers", "France"),
+        ("ent_ven", "1174 Ca' Foscari", "Venise", "Italie"),
+        ("ent_bak", "4658 Nakhon Sawhan Rd", "Bangkok", "Thaïlande");
+
+
+-- Ajoutez 10 expéditions différentes 
+insert into expeditions (date_expedition, date_livraison, id_entrepot_source, id_entrepot_destination, poids, statut)
+	values
+		("2022-03-03", "2022-03-05", 1, 2,  3.5, "livré"),
+        ("2022-01-15", "2022-01-24", 2, 5, 10.8, "livré"),
+        ("2023-02-25", null, 2, 4, 0.5, "expedié"),
+        ("2022-04-03", null, 3, 4, 0.2, "expedié"),
+        ("2022-09-11", null, 3, 5, 1.5, "expedié"),
+        ("2019-04-13", "2019-04-16", 5, 1, 3.5, "livré"),
+        ("2021-10-27", "2021-11-06", 4, 1, 7.2, "livré"),
+        ("2020-06-08", "2020-07-01", 5, 1, 8.6,  "livré"),
+        ("2023-04-03", null, 1, 5, 4.2, "expedié"),
+        ("2022-10-29", "2022-10-31", 4, 3, 3.3, "livré");
--- table_creation.sql
+++ table_creation.sql
@@ -1,22 +1,22 @@
 -- Créez une table "entrepots"
 create table entrepots (
 id int primary key auto_increment,
-nom_entrepot varchar(30),
-adresse varchar(255),
-ville varchar(126),
-pays varchar(50)
+nom_entrepot varchar(30) not null,
+adresse varchar(255) not null,
+ville varchar(126) not null,
+pays varchar(50) not null
 );
 
 
 -- Créez une table "expeditions" 
 create table expeditions (
 id int primary key auto_increment,
-date_expedition date,
+date_expedition date not null,
 date_livraison date,
-id_entrepot_source int,
-id_entrepot_destination int,
-poids decimal,
-statut varchar(7),
+id_entrepot_source int not null,
+id_entrepot_destination int not null,
+poids decimal(5,1) not null,
+statut varchar(7) not null,
 
 constraint fk_expeditions_id_entrepot_source_entrepots_id 
 foreign key (id_entrepot_source)
