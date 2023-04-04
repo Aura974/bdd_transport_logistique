b11855f903d973169cfe183f0471bf6fed5bffb7
Aurore DUCAP <aurore.ducap@gmail.com>
2023-04-04 19:10:46 +0400

    new data + requetes_avancees (4,5,6)

--- requetes_avancees.sql
+++ requetes_avancees.sql
@@ -12,7 +12,23 @@ where exp.statut = "expedie"
 group by id;
 
 
--- Affichez les expéditions qui ont un poids supérieur à 4 kg et qui sont en transit.
+-- Affichez les expéditions qui ont un poids supérieur à 100 kg et qui sont en transit.
 select * from expeditions exp
-where exp.poids > 4
+where exp.poids > 100
 and exp.statut = "expedie";
+
+
+-- Affichez le nombre d'expéditions envoyées par chaque entrepôt.
+select count(*) as total_envoi, nom_entrepot from expeditions exp
+inner join entrepots ent on ent.id = exp.id_entrepot_source
+group by nom_entrepot;
+
+
+-- Affichez le nombre total d'expéditions en transit.
+select count(*) as total_expedie from expeditions
+where statut = "expedie";
+
+
+-- Affichez le nombre total d'expéditions livrées.
+select count(*) as total_livre from expeditions
+where statut = "livre";
