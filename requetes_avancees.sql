bf78bbd81fa5bf9ec50bb41a1027d2a589bae690
Aurore DUCAP <aurore.ducap@gmail.com>
2023-04-05 17:47:18 +0400

    exp par mois de l'année

--- requetes_avancees.sql
+++ requetes_avancees.sql
@@ -32,3 +32,27 @@ where statut = "expedie";
 -- Affichez le nombre total d'expéditions livrées.
 select count(*) as total_livre from expeditions
 where statut = "livre";
+
+
+-- Affichez le nombre total d'expéditions pour chaque mois de l'année en cours.
+SELECT month(date_expedition) as mois_exp,
+case
+	when month(date_expedition) = 1 then "janvier"
+    WHEN month(date_expedition) = 2 THEN 'février'
+	WHEN month(date_expedition) = 3 THEN 'mars'
+	WHEN month(date_expedition) = 4 THEN 'avril'
+	WHEN month(date_expedition) = 5 THEN 'mai'
+	WHEN month(date_expedition) = 6 THEN 'juin'
+	WHEN month(date_expedition) = 7 THEN 'juillet'
+	WHEN month(date_expedition) = 8 THEN 'août'
+	WHEN month(date_expedition) = 9 THEN 'septembre'
+	WHEN month(date_expedition) = 10 THEN 'octobre'
+	WHEN month(date_expedition) = 11 THEN 'novembre'
+	ELSE 'décembre'
+end as mois,
+year(date_expedition),
+count(*) AS total_expeditions
+FROM expeditions
+WHERE YEAR(date_expedition) = YEAR(now())
+GROUP BY mois_exp
+order by mois_exp asc;
